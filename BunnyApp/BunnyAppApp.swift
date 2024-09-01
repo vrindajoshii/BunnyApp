3a0badadb9f9a799a6b7af22933a1d0170021f6d
Vrinda Joshi <vrindajoshi781@gmail.com>
2024-09-01 20:07:57 +0100

    initial commit

---
+++ BunnyApp/BunnyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BunnyAppApp.swift
+//  BunnyApp
+//
+//  Created by Vrinda Joshi on 01/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BunnyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
