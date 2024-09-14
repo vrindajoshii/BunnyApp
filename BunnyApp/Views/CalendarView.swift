2d581e222d987b495b0b0fb7b58d331847888707
sahars03 <159436621+sahars03@users.noreply.github.com>
2024-09-14 14:15:51 +0100

    Update CalendarView.swift

--- BunnyApp/Views/CalendarView.swift
+++ BunnyApp/Views/CalendarView.swift
@@ -2,40 +2,66 @@
 //  CalendarView.swift
 //  BunnyApp
 //
-//  Created by Dr M Sohail on 12/09/2024.
+//  Created by sahars03 on 12/09/2024.
 //
 
+import FirebaseFirestore
 import SwiftUI
 
 struct CalendarView: View {
     
-    @StateObject var viewModel = CalendarViewViewModel()
-
+    @StateObject var viewModel: CalendarViewViewModel
+    @FirestoreQuery var items: [ToDoItem]
+    @State var selectedDate = Date()
+            
+    init(userId: String) {
+//        self.userId = userId
+        let startOfDay = Calendar.current.startOfDay(for: Date())
+        let endOfDay = Calendar.current.date(byAdding: .day, value: 1, to: startOfDay)!
+        
+        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos",
+                                     predicates: [
+                                         .whereField("dueDate", isGreaterThanOrEqualTo: startOfDay.timeIntervalSince1970),
+                                         .whereField("dueDate", isLessThanOrEqualTo: endOfDay.timeIntervalSince1970)
+                                     ])
+        self._viewModel = StateObject(wrappedValue: CalendarViewViewModel(userId: userId))
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
                 DatePicker(
                     "Due Date",
-                    selection: $viewModel.currentDate,
+                    selection: $selectedDate,
                     displayedComponents: .date // Restrict to date only
                 )
-                .onChange(of: viewModel.currentDate) {
-                    viewModel.eventsOnDay(thisDay: viewModel.currentDate)
+                .onChange(of: selectedDate) {
+                    viewModel.eventsOnDay(thisDay: selectedDate)
                 }
                 .datePickerStyle(GraphicalDatePickerStyle())
                 
-                // here we can have code similar to ListView for listing the items under the calendar (without the delete button)
-                // do we use singleitemview again or something else
- //               List(items) { item in
- //                   SingleItemView(item: item, userId: viewModel.userId) // Pass userId
-//              }
+                List(viewModel.items) { item in
+                    listView(item: item)
+                }
+                .listStyle(PlainListStyle())
+                
             }
             .navigationTitle("Calendar")
             
         }
     }
+    
+    @ViewBuilder
+    func listView(item: ToDoItem) -> some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(item.title)
+                    .font(.body)
+                Text("\(Date(timeIntervalSince1970: item.dueDate).formatted(date: .abbreviated, time: .shortened))")
+                    .font(.footnote)
+                    .foregroundColor(Color(.secondaryLabel))
+            }
+        }
+    }
 }
 
-#Preview {
-    CalendarView()
-}
