4b0ca2bd7694c180d35a6e0f77f82d1c09e97f1e
sahars03 <159436621+sahars03@users.noreply.github.com>
2024-09-18 14:17:23 +0100

    Added notifications code

--- BunnyApp/Other/BunnyAppApp.swift
+++ BunnyApp/Other/BunnyAppApp.swift
@@ -1,14 +1,23 @@
 import SwiftUI
 import FirebaseCore
 import FirebaseFirestore
-
+import UIKit
+import UserNotifications
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
-
-    return true
+      
+      // Request notification permission
+      UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+          if granted {
+              print("Permission granted for notifications")
+          } else if let error = error {
+              print("Failed to get notification permission: \(error.localizedDescription)")
+          }
+      }
+      return true
   }
 }
 
